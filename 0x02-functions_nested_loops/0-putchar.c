ca47ae0cb6b6a0c20cf234500469347a0e959423
Altayeb21 <altayebmohamed2013@gmail.comgit config --global user.name Altayeb21>
2023-09-12 18:32:11 -0700

    k

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,6 +8,20 @@
  * On error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c)
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
+}
+
 {
 	return (write(1, &c, 1));
 }
