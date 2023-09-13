5e457f7ce5e16cf5ef7b16e9abb1afdd3c455b26
Altayeb21 <altayebmohamed2013@gmail.comgit config --global user.name Altayeb21>
2023-09-13 00:32:41 -0700

    k

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,19 +8,21 @@
 
 int print_sign(int n)
 {
+int o;
         if (n > 0)
         {
-                _putchar(43);
-                return (1);
+		_putchar('+');
+		o=(1);
         }
         else if (n == 0)
         {
-                _putchar(48);
-                return (0);
+                _putchar('0');
+		o=(0);
         }
         else if (n < 0)
         {
-                _putchar(45);
-                return (-1);
+                _putchar('-');
+                o=(-1);
         }
+	return (o)
 }
