6a917af6b81f715d7728b5c4d7e89b38eee5294f
Altayeb21 <altayebmohamed2013@gmail.comgit config --global user.name Altayeb21>
2023-09-12 20:14:15 -0700

    g

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,11 +1,10 @@
 #include "main.h"
 
 /**
- * print_sign - prints the sign of a number
- * @n: the int to check
- * Return: 1 for positive num, -1 for negative num or zero for anything else
+ * print_sign - Printys sum
+ * @n: The nmber
+ * Return: 1 for all
  */
-
 int print_sign(int n)
 {
 	if (n > 0)
@@ -13,15 +12,14 @@ int print_sign(int n)
 		_putchar(43);
 		return (1);
 	}
-	else if (n == 0)
-	{
-		_putchar(48);
-		return (0);
-	}
 	else if (n < 0)
 	{
 		_putchar(45);
 		return (-1);
 	}
-	_putchar("/n");
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
 }
