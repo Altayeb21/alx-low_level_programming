9ab6960a5f132f78ae02059ad859d4a0f4519947
Altayeb21 <altayebmohamed2013@gmail.comgit config --global user.name Altayeb21>
2023-09-14 08:29:42 -0700

    j

--- 0x04-more_functions_nested_loops/1-main.c
+++ 0x04-more_functions_nested_loops/1-main.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
  * main - check the code
  *
@@ -8,9 +9,10 @@
 int main(void)
 {
     char c;
+
     c = '0';
-    printf("%c: %d\n", c, _isdigit_betty(c));
+    printf("%c: %d\n", c, _isdigit(c));
     c = 'a';
-    printf("%c: %d\n", c, _isdigit_betty(c));
+    printf("%c: %d\n", c, _isdigit(c));
     return (0);
 }
