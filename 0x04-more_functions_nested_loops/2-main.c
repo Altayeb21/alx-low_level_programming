ef6b6eb0f793e9295c5bac8ed5c0a655d545d355
Altayeb21 <altayebmohamed2013@gmail.comgit config --global user.name Altayeb21>
2023-09-14 08:15:54 -0700

    j

---
+++ 0x04-more_functions_nested_loops/2-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("%d\n", mul_betty(98, 1024));
+	printf("%d\n", mul_betty(-402, 4096));
+	return (0);
+}
