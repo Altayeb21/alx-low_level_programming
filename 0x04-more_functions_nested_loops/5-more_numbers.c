cdf41f98fed7cacf93ce5bdf6327e9509f1b3119
Altayeb21 <altayebmohamed2013@gmail.comgit config --global user.name Altayeb21>
2023-09-14 20:14:42 -0700

    g

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,28 +1,21 @@
 #include "main.h"
+
 /**
- * more_numbers - Prints 10 times the numbers,
- *	from 0 to 14, followed by a new line.
- *	can only use _putchar three times
- *
- * Return: Always 0 (Success)
-*/
+ * more_numbers - print more numbers
+ */
 
 void more_numbers(void)
 {
-	int i;
+	int i, j;
 
-	for (i = 0; i < 10; i++)
+	for (i = 1; i <= 10; i++)
 	{
-		putchar('0' + i);
-		putchar('1' + i);
-		putchar('2' + i);
-		putchar('3' + i);
-		putchar('4' + i);
-		putchar('5' + i);
-		putchar('6' + i);
-		putchar('7' + i);
-		putchar('8' + i);
-		putchar('9' + i);
-		putchar('\n');
+		for (j = 0; j <= 14; j++)
+		{
+			if (j >= 10)
+				_putchar('1');
+			_putchar (j % 10 + '0');
+		}
+		_putchar('\n');
 	}
 }
